9db462cb1c40e6df694ef238f289e5732041c23c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-11-13 13:46:26 +0100

    Add custom version of runCLI with global support

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
+import os from 'os';
+import path from 'path';
+import fs from 'fs';
+import * as pine from '@pinefile/pine';
+
+const PINE_FILE_ORDER = Object.freeze([
+  'Pinefile',
+  'pinefile.js',
+  'pinefile.ts',
+]);
+
+const findGlobalFile = () => {
+  for (const file of PINE_FILE_ORDER) {
+    const p = path.join(os.homedir(), '.pine', file);
+    if (fs.existsSync(p)) {
+      return p;
+    }
+  }
+  return null;
+};
+
+const runCLI = (argv: string[]) => {
+  if (argv[0] === 'global') {
+    argv = argv.slice(1);
+    const file = findGlobalFile();
+    if (file) {
+      argv.push(`--file=${file}`);
+    }
+  }
+
+  return pine.api.runCLI(argv);
+};
+
 export * from '@pinefile/pine';
+export const api = {
+  ...pine.api,
+  runCLI,
+};
